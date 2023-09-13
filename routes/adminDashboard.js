2dcba864f4ebd87472581fcf9c37637e28dfd017
Raazi Muhammed K <70052135+raazi-muhammed@users.noreply.github.com>
2023-09-13 23:55:27 +0530

    v1.3
    - displayed user data to admin page
    - added delete operation

--- routes/adminDashboard.js
+++ routes/adminDashboard.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const collection = require("../model/mongodb");
 
-router.get("/", (req, res) => {
-	res.render("admin-dashboard");
+router.get("/", async (req, res) => {
+	const allUsers = await collection.find({});
+	console.log(allUsers);
+
+	res.render("admin-dashboard", { allUsers });
+});
+
+router.get("/delete", async (req, res) => {
+	console.log("deleting id: " + req.query.id);
+
+	await collection.deleteOne({ _id: req.query.id }); // returns {deletedCount: 1}
+
+	res.redirect("/admin/dashboard");
 });
 
 module.exports = router;
