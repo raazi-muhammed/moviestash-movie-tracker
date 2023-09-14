7bc8d55ddc6d033d93cbb8717b6e2bf8c5e1933e
Raazi Muhammed K <70052135+raazi-muhammed@users.noreply.github.com>
2023-09-14 14:25:33 +0530

    v2.0
    - add content to home with OMDB API

--- routes/userHomepage.js
+++ routes/userHomepage.js
@@ -1,13 +1,57 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.render("user-homepage");
+async function getMovie(movieId) {
+	try {
+		const response = await fetch(
+			`http://www.omdbapi.com/?i=${movieId}&apikey=81141d43`
+		);
+		const data = await response.json();
+
+		if (data.Response === "True") {
+			console.log(data.Poster + " " + data.Title);
+			const obj = {
+				poster: data.Poster,
+				title: data.Title,
+			};
+			return obj;
+		} else {
+			console.error("Movie not found");
+			return null;
+		}
+	} catch (error) {
+		console.error("Error fetching movie data:", error);
+		return null;
+	}
+}
+
+async function getAllMovies(moviesId) {
+	let moviesDetails = [];
+	for (const key in moviesId) {
+		console.log("movie all move:" + (await getMovie(moviesId[key])));
+		moviesDetails.push(await getMovie(moviesId[key]));
+	}
+	console.log(moviesDetails);
+	return moviesDetails;
+}
+
+router.get("/", async (req, res) => {
+	console.log("user home pages");
+	const moviesId = ["tt3896198", "tt4154796", "tt1517268", "tt15398776"];
+	// getMovies("tt3896198");
+	// getMovies("tt4154796");
+	// getMovies("tt1517268");
+	// getMovies("tt15398776");
+
+	moviesDetails = await getAllMovies(moviesId);
+	console.log("moviesdEtail: " + moviesDetails);
+	res.render("user-homepage", { moviesDetails });
 });
 
 router.get("/logout", (req, res) => {
 	console.log("Log Out");
 	req.session.destroy();
+
 	res.redirect("/");
 });
 
